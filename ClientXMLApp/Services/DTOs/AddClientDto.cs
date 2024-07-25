672a41f54fc6b54ef4aa12de8479eb6a6df1fb8c
Filip Vojnovski <c00l4l1f3@gmail.com>
2024-07-25 10:06:19 +0200

    add validators for addresses and clients

--- ClientXMLApp/Services/DTOs/AddClientDto.cs
+++ ClientXMLApp/Services/DTOs/AddClientDto.cs
@@ -5,6 +5,7 @@ namespace ClientXMLApp.Services.DTOs
     public class AddClientDto
     {
         [Required(ErrorMessage = "Name is required.")]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Birthdate is required.")]
--- ClientXMLApp/Services/DTOs/AddressDto.cs
+++ ClientXMLApp/Services/DTOs/AddressDto.cs
@@ -1,10 +1,15 @@
 ﻿using ClientXMLApp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ClientXMLApp.Services.DTOs
 {
     public class AddressDto
     {
+        [Required(ErrorMessage = "Address text is required.")]
+        [MinLength(5, ErrorMessage = "Address text must be at least 5 characters long.")]
         public string AddressText { get; set; }
+
+        [Required(ErrorMessage = "Address type is required.")]
         public AddressType Type { get; set; }
     }
 }
